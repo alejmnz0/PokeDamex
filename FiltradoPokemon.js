63fefee479cec648a27915387db4b538ef11d99c
A.Jimenez <jimenezalejandro505@gmail.com>
2023-10-03 12:27:01 +0200

    Terminado el aside de filtrado

---
+++ FiltradoPokemon.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    console.log("hola")
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/type',
+        type: 'GET'
+    }).done(function (resp) {
+        var listaTipos = resp.results;
+        listaTipos.forEach(function (tipo) {
+
+            var template = `
+            <li class="w-100">
+            <a href="#" class="nav-link link-light"> <span
+                    class="d-none d-sm-inline">${tipo.name}</span>
+            </a>
+            </li>`;
+            $('#submenu1').append(template);
+        })
+    })
+
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/generation',
+        type: 'GET'
+    }).done(function (resp) {
+        var listaGen = resp.results;
+        listaGen.forEach(function (gen) {
+
+            var template = `
+            <li class="w-100">
+            <a href="#" class="nav-link link-light"> <span
+                    class="d-none d-sm-inline">${gen.name}</span>
+            </a>
+            </li>`;
+            $('#submenu2').append(template);
+        })
+    })
+
+
+})
