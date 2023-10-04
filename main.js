a28d43ffd35e6d2b8aa2cd013df526f29c390058
Marco <marco.pertegal.prieto@gmail.com>
2023-10-04 20:30:29 +0200

    Barras de estadisticas y correccion de errores

--- main.js
+++ main.js
@@ -48,15 +48,26 @@ $(document).ready(function () {
 
             $('#sprite').attr("src", response.sprites.front_default);
 
-
+            //ataque máximo es 200
             $("#attack").text(response.stats[1].base_stat);
             //Hace que la barra trabaje con valores de 0 a 200
             var width = (response.stats[1].base_stat / 200) * 100;
             $("#attack").css("width", width + "%");
-
+            //defensa máximo es 100
             $("#defense").text(response.stats[2].base_stat);
             var width = (response.stats[2].base_stat / 200) * 100;
             $("#defense").css("width", width + "%");
+
+            //hp máximo es 100
+            $("#health").text(response.stats[0].base_stat);
+            $("#health").css("width", response.stats[0].base_stat + "%");
+
+
+            $("#speed").text(response.stats[5].base_stat);
+            var width = (response.stats[5].base_stat / 200) * 100;
+            $("#speed").css("width", width + "%");
+
+
 
 
             // Lo último es abrir el modal
